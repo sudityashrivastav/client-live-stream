93b1d8ad99e4fc3cbdafa5518981d04183000cd8
sudityashrivastav <sudityakumar1@gmail.com>
2024-04-26 21:27:02 +0530

    error icon, route fix

--- sports-stream-live/script.js
+++ sports-stream-live/script.js
@@ -1,15 +1,15 @@
-
 async function getCID(eventId) {
 
-    const res = await fetch(`http://46.101.91.237:8085/api/live-stream-source?eventid=${eventId}`)
+    const res = await fetch(`https://get-channelid.vercel.app/cid?eventid=${eventId}`)
     const data = await res.json()
 
     if (data.eid) {
         return data.eid
     }
-    alert("invalid event id")
-}
 
+    document.body.innerHTML =  `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path d="M250.26 166.05L256 288l5.73-121.95a5.74 5.74 0 00-5.79-6h0a5.74 5.74 0 00-5.68 6z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path d="M256 367.91a20 20 0 1120-20 20 20 0 01-20 20z"/></svg>`
+    return false
+}
 
 async function baseFunction() {
     const mainVideoSRC = document.getElementById("main-video-source")
@@ -19,16 +19,16 @@ async function baseFunction() {
     const myeid = searchParams.get("eventid")
 
     if (!myeid) {
-        alert("eventid missing")
+        document.body.innerHTML =  `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path d="M250.26 166.05L256 288l5.73-121.95a5.74 5.74 0 00-5.79-6h0a5.74 5.74 0 00-5.68 6z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path d="M256 367.91a20 20 0 1120-20 20 20 0 01-20 20z"/></svg>`
         return
     }
 
     const streamURL = await getCID(myeid)
 
     if (streamURL) {
-        mainVideoSRC.src = "https://sports.hr08bets.in/m3u8-proxy?cid="+ streamURL
-    } else {
-        document.body.innerHTML = "invalid event id"
+        mainVideoSRC.src = "https://sports.hr08bets.in/m3u8-proxy?cid=" + streamURL
+    } else {     
+         document.body.innerHTML =  `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path d="M250.26 166.05L256 288l5.73-121.95a5.74 5.74 0 00-5.79-6h0a5.74 5.74 0 00-5.68 6z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path d="M256 367.91a20 20 0 1120-20 20 20 0 01-20 20z"/></svg>`;
         return
     }
     // Get the video element
